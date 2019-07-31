1d41e8e8f45ed1942f458ec78a242f2779a6ee0c
HouJie <h.j.13.new@gmail.com>
2019-07-31 19:52:24 +0800

    update : SICP 1.2.2

--- Solution-Python/SICP/1.2.2.py
+++ Solution-Python/SICP/1.2.2.py
@@ -20,22 +20,30 @@ def get_money_value(money_type):
 def count_change(amount):
     """
     递归的核心思路
-    n元的兑换方法 = n元的兑换方法（这一次不使用d面值的纸币进行兑换） + n-d元的兑换方法
+    n元的兑换方法 = n元的兑换方法（这一次不使用d面值的纸币进行兑换） +
+                    n-d元的兑换方法(这次用d面值的纸币进行兑换)
+
+    其实这里通过从从大到小排列面值并赋予一个编号，
+    然后根据编号情况表示本次兑换可以使用的纸币范围
+    eg：kinds_of_coins = 5            -> 1,2,3,4,5 可用
+        kinds_of_coins = 2            -> 1,2 可用 3,4,5种纸币本次选择不用来进行兑换
+
 
     递归终止条件
     n < 0 或者没有可用纸币类型进行兑换    ->    0
     n = 0                            ->     1
     """
+
     def cc(am, kinds_of_coins):
-        if am < 0 or kinds_of_coins == 0:
+        if am < 0 or kinds_of_coins > 5:
             return 0
         elif am == 0:
             return 1
         else:
             return cc(am - get_money_value(kinds_of_coins), kinds_of_coins) + \
-                   cc(am, kinds_of_coins - 1)
+                   cc(am, kinds_of_coins + 1)
 
-    return cc(amount, 5)
+    return cc(amount, 1)
 
 
 if __name__ == '__main__':
